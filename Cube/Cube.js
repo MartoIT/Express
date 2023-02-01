4a274f26a5fda77e755dfbf75237b737e4f2b17c
MartoIT <88441320+MartoIT@users.noreply.github.com>
2023-02-01 15:52:27 +0200

    add db in json file

---
+++ Cube/Cube.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const db = require('../db.json');
+const path  = require('path');
+
+class Cube{
+    constructor(name, description, imageUrl, difficultyLevel){
+        
+    }
+}
