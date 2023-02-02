2cbd9b6e54d51f530469f93481d992239f9e23af
MartoIT <88441320+MartoIT@users.noreply.github.com>
2023-02-02 09:26:47 +0200

    add functionality to create page, now can add new cubes!

---
+++ controlers/createPage.js
@@ -0,0 +1,15 @@
+const Cube = require('../Cube/Cube');
+const db = require('../db.json');
+
+exports.cretePage = (req, res) => {
+    res.render('create');
+}
+
+exports.addCube = (req, res) => {
+    const {name, description, imageUrl, difficultyLevel} = req.body;
+
+    let cube = new Cube (name, description, imageUrl, difficultyLevel);
+
+    cube.save();
+    res.redirect('/')
+}
--- routers.js
+++ routers.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
 
 const home = require('./controlers/homepage')
+const create = require('./controlers/createPage')
 
 router.get('/', home.getHomePage);
 router.get('/about', home.getAboutPage);
 router.get('/404', home.get404Page);
+
+router.get('/create', create.cretePage);
+router.post('/create', create.addCube);
+
 
 
 module.exports = router;
