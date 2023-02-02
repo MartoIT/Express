41e328e4487fe3ddc4d2f0d431c2afca4baedb31
MartoIT <88441320+MartoIT@users.noreply.github.com>
2023-02-02 10:30:59 +0200

    the search bar i working and also the difficulty level is search it is work

--- controlers/homepage.js
+++ controlers/homepage.js
@@ -1,10 +1,20 @@
 const db = require('../db.json')
 
 exports.getHomePage = (req, res) => {
-    
+    const {search, from: difficultyFrom, to: difficultyTo} =  req.query;
     let cubes = db.cubes;
+    if(search) {
+        cubes = cubes.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()))
+    }
 
-    res.render('home', {cubes});
+    if(difficultyFrom){
+        cubes = cubes.filter(cube => cube.difficultyLevel >= difficultyFrom);
+    }
+
+    if(difficultyTo) {
+        cubes = cubes.filter(cube => cube.difficultyLevel <= difficultyTo);
+    }
+    res.render('home', {cubes, difficultyFrom, difficultyTo});
 };
 
 exports.getAboutPage = (req, res) => {
