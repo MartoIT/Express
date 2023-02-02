f730222ba301ed715776df7d019caab7f56bf783
MartoIT <88441320+MartoIT@users.noreply.github.com>
2023-02-02 10:08:04 +0200

    add details for evry cube to show.

---
+++ controlers/detailPage.js
@@ -0,0 +1,15 @@
+const db = require('../db.json');
+
+exports.getDitails = (req, res) => {
+    
+    const id = Number(req.params.cubeId)
+
+    if(!id) {
+        return res.redirect('/404')
+    }
+
+    let cube = db.cubes.find(x => x.id === id);
+
+    res.render('details', { cube })
+
+}
--- routers.js
+++ routers.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const home = require('./controlers/homepage')
 const create = require('./controlers/createPage')
+const details = require('./controlers/detailPage')
 
 router.get('/', home.getHomePage);
 router.get('/about', home.getAboutPage);
@@ -9,6 +10,8 @@ router.get('/404', home.get404Page);
 
 router.get('/create', create.cretePage);
 router.post('/create', create.addCube);
+
+router.get('/details/:cubeId', details.getDitails);
 
 
 
