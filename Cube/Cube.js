1d5cf22bab21e66bf0b10e6988d1456a934feaee
MartoIT <88441320+MartoIT@users.noreply.github.com>
2023-02-06 17:21:04 +0200

    add mongoose Schema and mongoose model

--- Cube/Cube.js
+++ Cube/Cube.js
@@ -1,21 +1,23 @@
-const fs = require('fs');
-const db = require('../db.json');
-const path  = require('path');
+const mongoose = require('mongoose');
 
-class Cube{
-    constructor(name, description, imageUrl, difficultyLevel){
-        this.name = name;
-        this.description = description;
-        this.imageUrl = imageUrl;
-        this.difficultyLevel = difficultyLevel;
+const cubeSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    imageUrl:{
+        type: String,
+        required: true,
+    },
+    difficultyLevel:{
+        type: Number,
+        required: true,
     }
+})
 
-    save(){
-        this.id = db.cubes[db.cubes.length -1].id + 1;
-        db.cubes.push(this);
-        const jsonData = JSON.stringify(db, null, 2);
-        fs.writeFileSync(path.resolve(__dirname, '../db.json'), jsonData);
-    }
-}
-
+const Cube = mongoose.model('Cube', cubeSchema);
 module.exports = Cube;
