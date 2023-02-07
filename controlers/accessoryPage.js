55d33671816be2a068223daeb6366862bb2d534f
MartoIT <88441320+MartoIT@users.noreply.github.com>
2023-02-07 16:14:19 +0200

    add some coments

--- controlers/accessoryPage.js
+++ controlers/accessoryPage.js
@@ -1,10 +1,11 @@
 const Accessory = require('../Cube/Accessory');
+const Cube = require('../Cube/Cube');
 
-exports.createAccessoryPage = (req, res) => {
+exports.createAccessoryPage = (req, res) => {//here show the page taht give you options to add new accessory.
     res.render('accessoryCreate')
 };
 
-exports.createAccessory = async ( req, res) => {
+exports.createAccessory = async ( req, res) => {//here add only accesory. To add accessory go no navbar and hit Add accessory.
     const { name, imageUrl, description} = req.body;
     try {
         await Accessory.create({name, imageUrl, description});
@@ -17,6 +18,10 @@ exports.createAccessory = async ( req, res) => {
    
 };
 
-exports.attach = (req, res) =>{
+exports.attach = async (req, res) =>{//went go to some of products detail page you
+    // can see button for add accesory, this function is render static page (just for now)
+    // for the product whith posible accesories to be add.
+    const cube = await Cube.findById
+
     res.render('attachAccessory');
 };
